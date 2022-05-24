1e71705d87fda806eb28760f2ce822bf5b88532f
roy ayon <ayon.roy255@gmail.com>
2022-05-25 00:52:28 +0600

    First Commit

---
+++ CV/Controllers/CvController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PersonalBackground()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
